37d33c958723ed00dd7b76f92b4111bca98a1a43
FranInfante <infante.portero@gmail.com>
2025-04-26 18:03:21 +0200

    cleanup

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from './modules/tasks/tasks.component';
-import { LandingComponent } from './modules/landing/landing.component';
 
-export const routes: Routes = [
-    {path: "", component: LandingComponent},
-    {path: "tasks", component: TasksComponent}
-];
+export const routes: Routes = [];
